4011edc00ca175ee8274feb5da702005272e1a45
xbsheng <xxbsheng@gmail.com>
2023-02-21 16:38:31 +0800

    feat: add env

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+console.log(`VUE_APP_PRODUCT: ${process.env.VUE_APP_PRODUCT}`)
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
